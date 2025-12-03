27c802ecb41f2954686b5b44d563325c997b5b37
Lokendra singh <Lokendra.singh2024@gmail.com>
2025-12-04 01:55:31 +0530

    Fix CORS for Vercel deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,8 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
 }));
 app.use(express.json());
 
